66deaf27e61ada44c5289157bc61e2b1a568c091
BlueMan2017 <s.k.tomaszewska@gmail.com>
2019-05-14 21:39:50 +0200

    add gallery from Unsplash API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 // import Header from "./components/js/Header";
-import Gallery from "./components/js/Gallery";
+// import Gallery from "./components/js/Gallery";
+import UnsplashGallery from "./components/js/Unsplash";
 
 function App() {
   return (
-    <div>
-      Hello
-      <Gallery />
+    <div className="gallery">
+      <UnsplashGallery />
     </div>
   );
 }
--- src/components/js/Gallery.js
+++ src/components/js/Gallery.js
@@ -1,74 +1,44 @@
 import React, { Component } from "react";
 import Unsplash from "unsplash-js";
-const numItemsToGenerate = 20; //how many gallery items you want on the screen
-const imageWidth = 480; //your desired image width in pixels
-const imageHeight = 480; //desired image height in pixels
-const collectionID = 1163637; //the collection ID from the original url
 
-// const myUnsplashId =
-//   "c23b06f5553b06b704c0af317f7f9490e88712216180d9162de3e355ac3d602d";
+const unsplash = new Unsplash({
+  applicationId:
+    "c23b06f5553b06b704c0af317f7f9490e88712216180d9162de3e355ac3d602d",
+  secret: "b5252deb184a488ccaff8789180ec47e07d77506cc1ce40f1d6a0f952cb02cbe"
+});
+
+unsplash.photos
+  .getRandomPhoto({ username: "naoufal" })
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+  });
 
 class Gallery extends Component {
   state = {
-    cars: "",
-    erondu: "",
+    myKey: "c23b06f5553b06b704c0af317f7f9490e88712216180d9162de3e355ac3d602d",
+    photos: [],
     error: false
   };
 
+  getUnsplash = () => {
+    fetch(`https://api.unsplash.com/photos/?client_id=${this.state.myKey}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log("Success:", data);
+      });
+  };
+
   componentDidMount() {
-    this.getCar();
+    this.getUnsplash();
   }
 
-  getCar = () => {
-    fetch("http://localhost:3000/cars")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          cars: data,
-          error: false
-        });
-      })
-      .catch(msg => {
-        console.log(msg);
-        this.setState({
-          error: true
-        });
-      });
-  };
-
-  getUnsplash = () => {
-    fetch(
-      "https://source.unsplash.com/user/erondu"
-      // `https://api.unsplash.com/photos/?client_id=${myUnsplashId}/photos/random`
-    )
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          erondu: data,
-          error: false
-        });
-      });
-  };
-
-  renderGalleryItem = () => {
-    fetch(
-      `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/`
-    ).then(response => {
-      let galleryItem = document.createElement("div");
-      galleryItem.classList.add("gallery-item");
-      galleryItem.innerHTML = `
-      <img class="gallery-image" src="${response.url}" alt="gallery image"/>
-    `;
-      document.body.appendChild(galleryItem);
-    });
-  };
-
   render() {
-    console.log(this.state.cars);
-    console.log(this.state.erondu);
-    if (this.state.error) {
-      return <h1>Blad komunikacji z serwerem</h1>;
-    }
+    // console.log(this.state.photos);
+    // console.log(this.state.erondu);
+    // if (this.state.error) {
+    //   return <h1>Blad komunikacji z serwerem</h1>;
+    // }
 
     return <button onClick={this.getCar} />;
   }

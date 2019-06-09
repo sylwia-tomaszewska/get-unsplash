584d9ab2cfca7788e0b4523752e3a9c736660fbf
BlueMan2017 <s.k.tomaszewska@gmail.com>
2019-06-09 03:59:27 +0200

    Updates

---
+++ precache-manifest.48dd6c2e349f1501471440936683bb11.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0096f6ad5f0f4c9dade1e03f586a29",
+    "url": "/get-unsplash/index.html"
+  },
+  {
+    "revision": "8a982c03c03d3b1f0d07",
+    "url": "/get-unsplash/static/css/main.bd14f831.chunk.css"
+  },
+  {
+    "revision": "e3cedbe0188a767d410c",
+    "url": "/get-unsplash/static/js/2.b5ffbd4d.chunk.js"
+  },
+  {
+    "revision": "8a982c03c03d3b1f0d07",
+    "url": "/get-unsplash/static/js/main.42b27078.chunk.js"
+  },
+  {
+    "revision": "2e7d009e8c96dcddde00",
+    "url": "/get-unsplash/static/js/runtime~main.fce01377.js"
+  },
+  {
+    "revision": "367e41fedb892b050dfe594772148b2a",
+    "url": "/get-unsplash/static/media/Optiker-K.367e41fe.woff2"
+  },
+  {
+    "revision": "40cab9a521c5be90361f486e748ba11e",
+    "url": "/get-unsplash/static/media/Optiker-K.40cab9a5.otf"
+  },
+  {
+    "revision": "1a10050e4d5a6f0d4aba447e4a479d1a",
+    "url": "/get-unsplash/static/media/TomatoSoup-Regular.1a10050e.woff"
+  },
+  {
+    "revision": "15506383184752cca8eb450c4a01fd46",
+    "url": "/get-unsplash/static/media/github.15506383.svg"
+  },
+  {
+    "revision": "46c524afc33bb2383bab81c925456d79",
+    "url": "/get-unsplash/static/media/unsplash.46c524af.svg"
+  }
+]);
